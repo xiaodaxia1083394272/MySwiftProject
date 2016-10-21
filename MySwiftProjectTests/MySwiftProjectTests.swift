1833d733aa55b467e09dfcd07a63ca241807f92e
xiaoyang <1083394272@qq.com>
2016-10-21 23:53:52 +0800

    Initial Commit

---
+++ MySwiftProjectTests/MySwiftProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  MySwiftProjectTests.swift
+//  MySwiftProjectTests
+//
+//  Created by qiuhaodong_macmini on 16/10/21.
+//  Copyright © 2016年 qiuhaodong_macmini. All rights reserved.
+//
+
+import XCTest
+@testable import MySwiftProject
+
+class MySwiftProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
