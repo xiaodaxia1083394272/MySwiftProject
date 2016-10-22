982653f3e2af990dde70d5091c3f115cdfee7f90
xiaoyang <1083394272@qq.com>
2016-10-22 10:05:04 +0800

    add image

---
+++ MySwiftProject/Fail mark.h
@@ -0,0 +1,13 @@
+//
+//  Fail mark.h
+//  MySwiftProject
+//
+//  Created by qiuhaodong_macmini on 16/10/22.
+//  Copyright © 2016年 qiuhaodong_macmini. All rights reserved.
+//
+
+#ifndef Fail_mark_h
+#define Fail_mark_h
+
+
+#endif /* Fail_mark_h */
